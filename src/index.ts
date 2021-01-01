c1df3051a9efa9a7fee7d783cbc0cf165e122697
Patricio Gonzalez <patote.gonzalez@gmail.com>
2021-01-01 13:17:22 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,35 @@ app.post('/users', async(req: Request, res: Response) => {
 })
 
 // READ
+app.get('/users', async(_:Request, res: Response) => {
+    try {
+        const users = await User.find()
+        return res.status(200).json(users)
+    } catch(err){
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
 
 // UPDATE
+app.put('/users/:uuid', async(req: Request, res: Response) => {
+    const uuid = req.params.uuid
+    const {name, email, role } = req.body
+
+    try {
+        const user = await User.findOneOrFail({ uuid })
+        user.name = name || user.name
+        user.email = email || user.email
+        user.role = role || user.role
+        await user.save()
+        return res.json(user)
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+        
+    }
+})
 
 // DELETE
 
